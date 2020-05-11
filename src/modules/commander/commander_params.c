e6b049f4afd9d8d0fbb31d73290e9eea2afd8791
Silvan Fuhrer <silvan@auterion.com>
2020-05-11 11:42:40 +0200

    airspeed preflight checks: move param for max airspeed to arm to commander
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/commander/commander_params.c
+++ src/modules/commander/commander_params.c
@@ -899,3 +899,18 @@ PARAM_DEFINE_INT32(COM_MOT_TEST_EN, 1);
  * @increment 0.1
  */
 PARAM_DEFINE_FLOAT(COM_KILL_DISARM, 5.0f);
+
+/**
+ * Maximum airspeed allowed for arming.
+ *
+ * Deny arming if the current airspeed estimate (equivalent airspeed) is greater than this value.
+ * Excessive airspeed measurements on ground are either caused by wind or bad airspeed calibration.
+ *
+ * @group Commander
+ * @unit m/s
+ * @min 0.5
+ * @max 30.0
+ * @decimal 1
+ * @increment 0.1
+ */
+PARAM_DEFINE_FLOAT(COM_ARM_ARSP_MAX, 4.0f);
